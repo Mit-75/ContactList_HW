2fc6b51bcdd62e9fd81c1097acdb50a0e8355730
Dmitry Parkhomenko <oku_74@mail.ru>
2023-12-14 21:58:30 +0500

    Initial Commit

---
+++ ContactList_HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList_HW
+//
+//  Created by Dmitry Parhomenko on 14.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
